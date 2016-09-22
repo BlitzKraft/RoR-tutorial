9c12715ef2740a958a6c81a45c672acd0be9cd1e
BK Bolisetty <bk@blitzkraft.me>
2016-09-21 19:49:24 -0700

    End of session 2

---
+++ Pinterest/app/controllers/pins_controller.rb
@@ -0,0 +1,34 @@
+class PinsController < ApplicationController
+	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	def index
+	end
+	def show
+	end
+
+
+	def new
+		@pin = Pin.new
+	end
+
+	def create
+		@pin = Pin.new(pin_params)
+		if @pin.save
+			redirect_to @pin, notice: "Success!!"
+		else
+			render 'new'
+		end
+
+	end
+
+	def update
+	end
+
+	private
+	def pin_params
+		params.require(:pin).permit(:description, :title)
+	end
+
+	def find_pin
+		@pin = Pin.find(params[:id])
+	end
+end
--- Pinterest/config/routes.rb
+++ Pinterest/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+	resources :pins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root "pins#index"
+	get "/new", to: "pins#new"
+	get "/pins/:id", to:"pins#show"
 end
+
