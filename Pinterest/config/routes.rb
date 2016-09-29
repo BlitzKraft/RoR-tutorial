78c37a61aba72d189b5d6662467a959410d3e3b3
BK Bolisetty <bk@blitzkraft.me>
2016-09-28 19:54:43 -0700

    Added bootstrap and devise.

--- Pinterest/config/routes.rb
+++ Pinterest/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	resources :pins
+	resources :pins, :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root "pins#index"
 	get "/new", to: "pins#new"
--- Pinterest/db/schema.rb
+++ Pinterest/db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914232632) do
+ActiveRecord::Schema.define(version: 20160929024831) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
